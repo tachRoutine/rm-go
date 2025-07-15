8bb952b9a09c125fa45e5dabeb458ef337042f07
tacherasasi <tacherasasi@gmail.com>
2025-07-15 23:11:07 +0300

    Ellie: more code

--- main.go
+++ main.go
@@ -1,9 +1,31 @@
 package main
 
-import "flag"
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"os"
+	"strings"
+)
 
 func main() {
-	force := flag.Bool("f", false ,"falseforce deletion, ignore nonexistent files, no prompt")
+	force := flag.Bool("f", false, "falseforce deletion, ignore nonexistent files, no prompt")
 	interactive := flag.Bool("i", false, "prompt before every removal")
 	flag.Parse()
+	files := flag.Args()
+
+	if len(files) == 0 {
+		fmt.Fprintln(os.Stderr, "rm: missing operand")
+		os.Exit(1)
+		flag.Usage()
+		return
+	}
+}
+
+func confirm(file string) bool {
+	fmt.Printf("rm: remove '%s'? [y/N]: ", file)
+	reader := bufio.NewReader(os.Stdin)
+	answer, _ := reader.ReadString('\n')
+	answer = strings.TrimSpace(strings.ToLower(answer))
+	return answer == "y" || answer == "yes"
 }
