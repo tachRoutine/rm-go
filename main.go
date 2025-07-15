84693385fb4c30ec29c46eba4bb75bf7410af4fc
tacherasasi <tacherasasi@gmail.com>
2025-07-15 22:57:26 +0300

    Ellie: declared the flags

--- main.go
+++ main.go
@@ -1,4 +1,9 @@
 package main
 
+import "flag"
+
 func main() {
+	force := flag.Bool("f", false ,"falseforce deletion, ignore nonexistent files, no prompt")
+	interactive := flag.Bool("i", false, "prompt before every removal")
+	flag.Parse()
 }
